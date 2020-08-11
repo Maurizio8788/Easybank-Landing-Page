66b83e778c3a712cfff917e521e13927cd2b3204
Maurizio <mrz.strazzullo@gmail.com>
2020-08-11 17:51:14 +0200

    Express static add

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const easy_api = require('./api/easybank');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+/* file statici   */
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+/*  API call */
+
 app.get( '/api/easybank', function(req, res) {
     res.sendFile( __dirname + '/api/easybank.json' );
 })
 
+/*  SERVER  */
 
 app.listen( PORT, function(req, res){
     console.log(`Server is running on port ${PORT}` );
